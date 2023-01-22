df749e2a7a0fcaf038c1e2fc15ab653195f379a6
Bintang Aditya <bintangaditya676@gmail.com>
2023-01-23 02:04:45 +0700

    Add project to repo

---
+++ app/src/main/java/com/example/markerlessproject/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.markerlessproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentOnAttachListener;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Config;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Session;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.SceneView;
+import com.google.ar.sceneform.Sceneform;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.rendering.ViewRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.lang.ref.WeakReference;
+
+public class MainActivity extends AppCompatActivity
+        implements FragmentOnAttachListener, BaseArFragment.OnTapArPlaneListener,
+        BaseArFragment.OnSessionConfigurationListener, ArFragment.OnViewCreatedListener {
+
+    private ArFragment arFragment;
+    private Renderable renderable;
+    private ViewRenderable viewRenderable;
+    private Boolean mars = false;
+    private Boolean earth = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().addFragmentOnAttachListener(this);
+
+        if(savedInstanceState == null) {
+            if(Sceneform.isSupported(this)) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.arFragment, ArFragment.class, null)
+                        .commit();
+            }
+        }
+        mars = true;
+        loadModels();
+    }
+
+    @Override
+    public void onAttachFragment(@NonNull FragmentManager fragmentManager, @NonNull Fragment fragment) {
+        if (fragment.getId() == R.id.arFragment) {
+            arFragment = (ArFragment) fragment;
+            arFragment.setOnSessionConfigurationListener(this);
+            arFragment.setOnViewCreatedListener(this);
+            arFragment.setOnTapArPlaneListener(this);
+        }
+    }
+
+    @Override
+    public void onSessionConfiguration(Session session, Config config) {
+        if (session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)) {
+            config.setDepthMode(Config.DepthMode.AUTOMATIC);
+        }
+    }
+
+    @Override
+    public void onViewCreated(ArSceneView arSceneView) {
+        arFragment.setOnViewCreatedListener(null);
+        arSceneView.setFrameRateFactor(SceneView.FrameRate.FULL);
+    }
+
+    private void loadModels() {
+        WeakReference<MainActivity> weakReference = new WeakReference<>(this);
+        if (earth) {
+            ModelRenderable.builder()
+                    .setSource(this, Uri.parse("earth.glb"))
+                    .setIsFilamentGltf(true)
+                    .setAsyncLoadEnabled(true)
+                    .build()
+                    .thenAccept(model -> {
+                        MainActivity activity = weakReference.get();
+                        if (activity != null) {
+                            activity.renderable = model;
+                        }
+                    });
+        } else if (mars) {
+            ModelRenderable.builder()
+                    .setSource(this, Uri.parse("mars.glb"))
+                    .setIsFilamentGltf(true)
+                    .setAsyncLoadEnabled(true)
+                    .build()
+                    .thenAccept(model -> {
+                        MainActivity activity = weakReference.get();
+                        if (activity != null) {
+                            activity.renderable = model;
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+        if (renderable == null) {
+            Toast.makeText(this, "Loading...", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Anchor anchor = hitResult.createAnchor();
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+        TransformableNode model = new TransformableNode(arFragment.getTransformationSystem());
+        model.setParent(anchorNode);
+        model.setRenderable(this.renderable)
+                .animate(true).start();
+        model.select();
+    }
+}
